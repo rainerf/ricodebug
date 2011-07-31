71898e7af1e51d65b872faff0a2016f27ff7add9
rainer <rainer@findenig.net>
2011-07-31 12:04:22 +0200

    nicer quickwatch

--- src/controllers/quickwatch.py
+++ src/controllers/quickwatch.py
@@ -1,16 +1,17 @@
-from PyQt4.QtGui import QToolBar, QTextEdit
+from PyQt4.QtGui import QToolBar, QLineEdit, QIcon
 from PyQt4.QtCore import QObject
 
 class QuickWatch(QToolBar):
     def __init__(self, parent, distributed_objects):
         QObject.__init__(self, "QuickWatch")
+        self.setObjectName("QuickWatch")
         parent.addToolBar(self)
-        self.watchedit = QTextEdit()
+        self.watchedit = QLineEdit()
         self.watchedit.setFixedHeight(28)
         self.addWidget(self.watchedit)
         self.distributed_objects = distributed_objects
-        self.addAction("Add to Watch", self.addToWatch)
-        self.addAction("Add to Data Graph", self.addToDG)
+        self.addAction(QIcon(":/icons/images/watch.png"), "Add to Watch", self.addToWatch)
+        self.addAction(QIcon(":/icons/images/datagraph.png"), "Add to Data Graph", self.addToDG)
     
     def addToWatch(self):
         self.distributed_objects.watch_controller.addWatch(self.watchedit.toPlainText())
