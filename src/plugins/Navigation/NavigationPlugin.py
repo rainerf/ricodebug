41f0ed7a623b1d89ac894d7f726150008cef6f7e
rainer <rainer@findenig.net>
2012-02-20 21:43:07 +0100

    still cleaning

--- src/plugins/Navigation/NavigationPlugin.py
+++ src/plugins/Navigation/NavigationPlugin.py
@@ -6,62 +6,62 @@ import os
 
 from PyQt4.QtCore import QThread
 
+
 class CTagsRunner(QThread):
     def __init__(self, tmpFile, parent=None):
         super(CTagsRunner, self).__init__(parent)
         self.tmpFile = tmpFile
         self.sources = None
-    
+
     def oneshot(self, sources):
         self.sources = sources
         self.start()
-        
+
     def run(self):
         os.system('ctags --fields=afmikKlnsStz -f %s %s' % (self.tmpFile, " ".join(self.sources)))
         self.emit(QtCore.SIGNAL("tagsFileAvailable()"))
+
 
 class NavigationView(QtGui.QTreeView):
     def __init__(self, signalproxy, parent=None):
         QtGui.QTreeView.__init__(self, parent)
         self.signalproxy = signalproxy
         self.doubleClicked.connect(self.openEntry)
-    
+
     def openEntry(self, index):
         x = self.model().itemFromIndex(index).data(EntryItem.ENTRYROLE)
         self.signalproxy.openFile(x.file_, x.lineNumber)
-    
+
     def contextMenuEvent(self, e):
         index = self.currentIndex()
         if not index.isValid():
             return
-        
+
         x = self.model().itemFromIndex(index).data(EntryItem.ENTRYROLE)
         if not isinstance(x, Function):
             return
-        
+
         def addBreakpoint(breakpoint_controller, file_, line):
             def f():
                 breakpoint_controller.insertBreakpoint(file_, line)
             return f
-        
+
         menu = QtGui.QMenu()
         menu.addAction(QtGui.QIcon(":/icons/images/bp.png"), "Break on %s (%s:%s)" % (x.name, x.file_, x.lineNumber), addBreakpoint(self.signalproxy.distributedObjects.breakpointController, x.file_, x.lineNumber))
         menu.exec_(self.viewport().mapToGlobal(e.pos()))
         e.accept()
 
+
 class NavigationPlugin(QtCore.QObject):
     ''' CTags-based navigation plugin Widget '''
-    
-    # ================================= 
-    # functions called by pluginloader
-    # =================================
+
     def __init__(self):
         QtCore.QObject.__init__(self)
         self.dockwidget = None
-        
+
     def initPlugin(self, signalproxy):
         """Init function - called when pluginloader loads plugin."""
-        
+
         self.signalproxy = signalproxy
         self.entrylist = EntryList()
 
@@ -69,32 +69,31 @@ class NavigationPlugin(QtCore.QObject):
         self.dockwidget = QtGui.QDockWidget(None)
         self.dockwidget.setObjectName("Navigation")
         self.dockwidget.setWindowTitle(QtGui.QApplication.translate("MainWindow", "Navigation", None, QtGui.QApplication.UnicodeUTF8))
-        
+
         self.view = NavigationView(self.signalproxy)
         self.view.setModel(self.entrylist.model)
         self.dockwidget.setWidget(self.view)
-        
+
         # add widget to mainwindow
         self.signalproxy.addDockWidget(Qt.BottomDockWidgetArea, self.dockwidget)
         QtCore.QObject.connect(self.signalproxy.distributedObjects.debugController, QtCore.SIGNAL('executableOpened'), self.update)
-        
+
         self.ctagsRunner = CTagsRunner("%s/tags%d" % (str(QtCore.QDir.tempPath()), os.getpid()))
         QtCore.QObject.connect(self.ctagsRunner, QtCore.SIGNAL("tagsFileAvailable()"), self.tagsFileReady, Qt.QueuedConnection)
-        
+
         # load the tags if the plugin was loaded after the executable
         self.update()
-        
+
     def deInitPlugin(self):
         """Deinit function - called when pluginloader unloads plugin."""
         self.dockwidget.close()
         self.signalproxy.removeDockWidget(self.dockwidget)
-        
+
     def update(self):
         sources = self.signalproxy.distributedObjects.gdb_connector.getSources()
         if len(sources) > 0:
             self.ctagsRunner.oneshot(sources)
-        
+
     def tagsFileReady(self):
         self.entrylist.readFromFile(self.ctagsRunner.tmpFile)
         os.remove(self.ctagsRunner.tmpFile)
-
--- src/plugins/Navigation/ctags_support.py
+++ src/plugins/Navigation/ctags_support.py
@@ -1,6 +1,7 @@
 import ctags
-from PyQt4.QtGui import QApplication, QStandardItemModel, QStandardItem, QPixmap, QIcon
+from PyQt4.QtGui import QStandardItemModel, QStandardItem, QPixmap, QIcon
 from PyQt4.QtCore import Qt
+
 
 class EntryList:
     def __init__(self, filename=None):
@@ -8,25 +9,25 @@ class EntryList:
         self.topLevelEntries = []
         self.model = QStandardItemModel()
         self.model.setHorizontalHeaderLabels(["Name", "File", "Line", "Type"])
-        
+
         if filename:
             self.readFromFile(filename)
-        
+
     def readFromFile(self, filename):
         self.scopes = {}
         self.topLevelEntries = []
         self.model.clear()
         self.model.setHorizontalHeaderLabels(["Name", "File", "Line", "Type"])
-        
+
         e = ctags.TagEntry()
         tf = ctags.CTags(filename)
         while tf.next(e):
             self.addFromTagEntry(e)
-    
+
     def getScope(self, scope):
         hier = scope.split("::")
         for s in range(len(hier)):
-            scopename = "::".join(hier[0:s+1])
+            scopename = "::".join(hier[0:s + 1])
             if scopename not in self.scopes:
                 self.scopes[scopename] = Struct(self)
         return self.scopes[scope]
@@ -42,7 +43,7 @@ class EntryList:
             scope = e['class']
         else:
             scope = None
-        
+
         # Only add a class/struct if it wasn't present in the file before; this
         # can happen if there are template specializations such as myclass and
         # myclass<T>, which are both represented as myclass in the file. If the
@@ -50,13 +51,13 @@ class EntryList:
         # heuristic ;).
         add = True
         if kind == "class":
-            n = self.getScope(scope+"::"+name if scope else name)
+            n = self.getScope(scope + "::" + name if scope else name)
             if n.lineNumber == None:
                 n.setValues(name, file_, lineNumber, scope, Struct.CLASS)
             else:
                 add = False
         elif kind == "struct":
-            n = self.getScope(scope+"::"+name if scope else name)
+            n = self.getScope(scope + "::" + name if scope else name)
             if n.lineNumber == None:
                 n.setValues(name, file_, lineNumber, scope, Struct.STRUCT)
             else:
@@ -64,7 +65,7 @@ class EntryList:
         elif kind == "function":
             n = Function(self)
             n.setValues(name, file_, lineNumber, scope, e['signature'])
-        elif kind == "member" and scope: # namespace members are declared as member, but don't have a scope
+        elif kind == "member" and scope:    # namespace members are declared as member, but don't have a scope
             n = Member(self)
             n.setValues(name, file_, lineNumber, scope, {"private": Member.PRIVATE, "protected": Member.PROTECTED, "public": Member.PUBLIC, None: None}[e['access']])
         else:
@@ -73,18 +74,21 @@ class EntryList:
         if add:
             n.addToParent()
 
+
 class EntryItem(QStandardItem):
     ENTRYROLE = Qt.UserRole + 1
+
     def __init__(self, entry):
         QStandardItem.__init__(self)
         self.entry = entry
-    
+
     def data(self, role):
         ret = QStandardItem.data(self, role)
         if role == self.ENTRYROLE:
             return self.entry
         else:
             return ret
+
 
 class Entry:
     def __init__(self, entrylist):
@@ -94,14 +98,14 @@ class Entry:
         self.file_ = None
         self.lineNumber = None
         self.items = [EntryItem(self), EntryItem(self), EntryItem(self), EntryItem(self)]
-        
+
     def setValues(self, name, file_, lineNumber, scope):
         self.name = name
         self.file_ = file_
         self.lineNumber = lineNumber
         assert not self.scope
         self.scope = scope
-        
+
         self.items[0].setText(name)
         self.items[0].setIcon(QIcon(QPixmap(":/icons/images/var.png")))
         self.items[1].setText(file_)
@@ -109,7 +113,7 @@ class Entry:
         self.items[3].setText("")
         for i in self.items:
             i.setEditable(False)
-            
+
     def addToParent(self):
         if self.scope:
             self.entrylist.getScope(self.scope).children.append(self)
@@ -117,28 +121,33 @@ class Entry:
         else:
             self.entrylist.topLevelEntries.append(self)
             self.entrylist.model.invisibleRootItem().appendRow(self.items)
-    
+
     def __str__(self):
         return "%s %s %s @ %s:%d" % (self.__class__, self.scope, self.name, self.file_, self.lineNumber)
 
+
 class Struct(Entry):
     STRUCT, CLASS = range(2)
+
     def __init__(self, entrylist):
         Entry.__init__(self, entrylist)
         self.children = []
-    
+        self.kind = None
+
     def setValues(self, name, file_, lineNumber, scope, kind):
         Entry.setValues(self, name, file_, lineNumber, scope)
         self.kind = kind
         self.items[0].setIcon(QIcon(QPixmap(":/icons/images/struct.png")))
         self.items[3].setText(["struct", "class"][kind])
-    
+
     def __str__(self):
         return "\n".join([Entry.__str__(self)] + ["    " + str(c).replace("\n", "\n    ") for c in self.children])
+
 
 class Function(Entry):
     def __init__(self, entrylist):
         Entry.__init__(self, entrylist)
+        self.signature = None
 
     def setValues(self, name, file_, lineNumber, scope, signature):
         Entry.setValues(self, name, file_, lineNumber, scope)
@@ -147,33 +156,16 @@ class Function(Entry):
         self.items[0].setIcon(QIcon(QPixmap(":/icons/images/sc_process.png")))
         self.items[3].setText("Function")
 
+
 class Member(Entry):
     PRIVATE, PROTECTED, PUBLIC = range(3)
+
     def __init__(self, entrylist):
         Entry.__init__(self, entrylist)
         self.items[3].setText("Member")
+        self.access = None
 
     def setValues(self, name, file_, lineNumber, scope, access):
         Entry.setValues(self, name, file_, lineNumber, scope)
         self.access = access
         self.items[0].setIcon(QIcon(QPixmap(":/icons/images/var.png")))
-
-def test():
-    def viewDoubleClicked(index):
-        file_ = index.sibling(index.row(), 1).data().toString()
-        line = index.sibling(index.row(), 2).data().toInt()[0]
-        print file_, line
-    
-    import sys
-    from PyQt4.QtGui import QTreeView
-    from PyQt4.QtCore import QObject, SIGNAL
-    app = QApplication(sys.argv)
-    widget = QTreeView(None)
-    QObject.connect(widget, SIGNAL("doubleClicked(QModelIndex)"), viewDoubleClicked)
-    entrylist = EntryList("/home/rainer/tmp/tags")
-    widget.setModel(entrylist.model)
-    widget.show()
-    sys.exit(app.exec_())
-
-if __name__ == '__main__':
-    test()
