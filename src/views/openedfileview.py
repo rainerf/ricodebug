18b3e10066d9fd4dc97475bc63c549de27c42cb4
Alexander Preisinger <alexander.preisinger@gmail.com>
2012-05-15 09:29:09 +0200

    openedfileview: use new connect style

--- src/views/openedfileview.py
+++ src/views/openedfileview.py
@@ -26,13 +26,14 @@ import re
 import cgi
 from PyQt4 import QtCore, QtGui, Qsci
 from PyQt4.QtGui import QPixmap, QIcon, QToolTip, QFont, QColor
-from PyQt4.QtCore import SIGNAL, QObject, Qt
+from PyQt4.QtCore import QObject, Qt
 from math import log, ceil
 from helpers.actions import Actions, ActionEx
 import logging
 
 
 class OpenedFileView(QObject):
+    
     MARGIN_NUMBERS, MARGIN_MARKER_BP, MARGIN_MARKER_TP, MARGIN_MARKER_EXEC, \
     MARGIN_MARKER_EXEC_SIGNAL, MARGIN_MARKER_STACK = range(6)
 
@@ -104,7 +105,7 @@ class OpenedFileView(QObject):
         self.file_.close()
 
         self.changed = False
-        self.connect(self.edit, SIGNAL("modificationChanged(bool)"), self.__setFileModified)
+        self.edit.modificationChanged.connect(self.__setFileModified)
 
         self.setMarginWidthByLineNumbers()
         self.edit.SendScintilla(Qsci.QsciScintilla.SCI_SETMOUSEDWELLTIME, 500)
@@ -112,29 +113,23 @@ class OpenedFileView(QObject):
         # override scintillas context menu with our own
         self.edit.SendScintilla(Qsci.QsciScintilla.SCI_USEPOPUP, 0)
         self.edit.setContextMenuPolicy(Qt.CustomContextMenu)
-        self.connect(self.edit, SIGNAL('customContextMenuRequested(QPoint)'), self.showContextMenu)
+        self.edit.customContextMenuRequested.connect(self.showContextMenu)
 
-        self.connect(self.edit, SIGNAL("marginClicked(int,int,Qt::KeyboardModifiers)"), self.marginClicked)
-        self.connect(self.edit, SIGNAL("SCN_DOUBLECLICK(int, int, int)"), self.editDoubleClicked)
-        self.connect(self.edit, SIGNAL("SCN_DWELLSTART(int, int, int)"), self.dwellStart)
-        self.connect(self.edit, SIGNAL("SCN_DWELLEND(int, int, int)"), self.dwellEnd)
+        self.edit.marginClicked.connect(self.marginClicked)
+        self.edit.SCN_DOUBLECLICK.connect(self.editDoubleClicked)
+        self.edit.SCN_DWELLSTART.connect(self.dwellStart)
+        self.edit.SCN_DWELLEND.connect(self.dwellEnd)
 
         # initially, read all breakpoints and tracepoints from the model
         self.getBreakpointsFromModel()
         self.getTracepointsFromModel()
 
-        self.connect(self.breakpointController.model(),
-                SIGNAL('rowsInserted(QModelIndex, int, int)'),
-                self.getBreakpointsFromModel)
-        self.connect(self.breakpointController.model(),
-                SIGNAL('rowsRemoved(QModelIndex, int, int)'),
-                self.getBreakpointsFromModel)
-        self.connect(self.tracepointController.model(),
-                SIGNAL('rowsInserted(QModelIndex, int, int)'),
-                self.getTracepointsFromModel)
-        self.connect(self.tracepointController.model(),
-                SIGNAL('rowsRemoved(QModelIndex, int, int)'),
-                self.getTracepointsFromModel)
+        _model = self.breakpointController.model()
+        _model.rowsInserted.connect(self.getBreakpointsFromModel)
+        _model.rowsRemoved.connect(self.getBreakpointsFromModel)
+        _model = self.tracepointController.model() 
+        _model.rowsInserted.connect(self.getTracepointsFromModel)
+        _model.rowsRemoved.connect(self.getTracepointsFromModel)
 
         Actions.AddWatch.triggered.connect(self.addWatch)
         Actions.ToggleTrace.triggered.connect(self.toggleTracepoint)
