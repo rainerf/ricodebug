rainerf/ricodebug

commit 23303d2d6d5bbe502e1eeb53366196b5ff331e70

simplified wrappers; fixed the child format for pointers * removed get/set functions, use the member or properties where needed * directly delegate all value/type/exp accesses to the variable without   explicit indirection * drop the unneeded ctors for the variables

src/datagraph/arrayvariableview.py
@@ -117,7 +117,7 @@ class ArrayVariableTemplateHandler(ComplexTemplateHandler):
         # we only allow the graphical view if all contained elements are standard variables; also,
         # do not show the menu if the variable view is collapsed
         if self.varWrapper.isOpen and graphicalViewPossible:
-            action = menu.addAction(QIcon(":/icons/images/graph.png"), "Graphical view for %s" % self.varWrapper.getExp(), self.toggleGraphicalView)
+            action = menu.addAction(QIcon(":/icons/images/graph.png"), "Graphical view for %s" % self.varWrapper.exp, self.toggleGraphicalView)
             action.setCheckable(True)
             action.setChecked(self.graphicalView)
 

src/controllers/treeitemcontroller.py
@@ -50,16 +50,15 @@ class TreePtrVarWrapper(VariableWrapper, TreeItem):
         @param factory   derived from VarWrapperFactory, factory to look in VariableList for children
         """
         if (self.getChildCount() == 0):
-            variable = self.variable.dereference()
-            if variable != None:
-                children = variable._getChildItems()
-                if (len(children) == 0):
+            variable = self._v.dereference()
+            if variable is not None:
+                if not variable.hasChildren:
                     vwChild = variable.makeWrapper(factory)
                     vwChild.parent = self
                     vwChild.dataChanged.connect(vwChild.hasChanged)
                     self.addChild(vwChild)
                 else:
-                    for child in children:
+                    for child in variable.childs:
                         vwChild = child.makeWrapper(factory)
                         vwChild.parent = self
                         vwChild.dataChanged.connect(vwChild.hasChanged)
@@ -91,8 +90,8 @@ class TreeStructVarWrapper(VariableWrapper, TreeItem):
             Get Children from VariableList for StructVariable
         @param factory   derived from VarWrapperFactory, factory to look in VariableList for children
         """
-        if (self.childItems.__len__() == 0):
-            for child in self.variable.getChildren():
+        if len(self.childItems) == 0:
+            for child in self._v.childs:
                 vwChild = child.makeWrapper(factory)
                 vwChild.parent = self
                 vwChild.dataChanged.connect(vwChild.hasChanged)

src/controllers/watchcontroller.py
@@ -65,7 +65,7 @@ class WatchController(TreeItemController):
         """
         watchParent = xmlHandler.createNode("Watches")
         for var in self.variableModel.getVariables():
-            xmlHandler.createNode("Watch", watchParent, {'exp': var.getExp()})
+            xmlHandler.createNode("Watch", watchParent, {'exp': var.exp})
 
     def loadSession(self, xmlHandler):
         """ load session info to xml file

src/datagraph/datagraphcontroller.py
@@ -150,10 +150,10 @@ class DataGraphController(QObject):
         """
         dgWatches = xmlHandler.createNode("GraphWatches")
         for vw in self.variableList:
-            xmlHandler.createNode("Watch", dgWatches, {'expression': vw.getExp(), 'xPos': vw.getXPos(), 'yPos': vw.getYPos()})
+            xmlHandler.createNode("Watch", dgWatches, {'expression': vw.exp, 'xPos': vw.getXPos(), 'yPos': vw.getYPos()})
         #dgPointers = xmlHandler.createNode("Pointers")
         #for pointer in self.pointerList:
-        #    xmlHandler.createNode("Pointer", dgPointers, { 'expFrom': pointer.fromView.var.getExp(), 'expTo': pointer.toView.var.getExp() })
+        #    xmlHandler.createNode("Pointer", dgPointers, { 'expFrom': pointer.fromView.var.exp, 'expTo': pointer.toView.var.exp })
 
     def loadSession(self, xmlHandler):
         """ load session info to xml file
@@ -184,10 +184,10 @@ class DataGraphController(QObject):
 #    def getVarByWatch(self, watch):
 #        """
 #        @param watch    string, the watch-Expression of the desired Variable
-#        @return         variables.variable.Variable, the desired Variable with var.getExp() == watch
+#        @return         variables.variable.Variable, the desired Variable with var.exp == watch
 #        """
 #        for var in self.variableList:
-#            if var.getExp() == watch:
+#            if var.exp == watch:
 #                return var
 #        # in case that no var was found, return None
 #        return None

src/datagraph/datagraphvw.py
@@ -75,7 +75,7 @@ class HtmlTemplateHandler(QObject):
 
     @QtCore.pyqtSlot(str)
     def setValue(self, value):
-        self.varWrapper.variable.setValue(value)
+        self.varWrapper.assignValue(value)
 
     def prepareContextMenu(self, menu):
         menu.addSeparator()
@@ -83,7 +83,7 @@ class HtmlTemplateHandler(QObject):
 
     # insert a "header" into the menu for the current element
     def addContextMenuLabel(self, menu):
-        label = QLabel("Actions for %s" % (self.varWrapper.getExp()))
+        label = QLabel("Actions for %s" % (self.varWrapper.exp))
         label.setStyleSheet("color:palette(light); background-color:palette(dark); margin-top:2px; margin-bottom:2px; margin-left:2px; margin-right:2px;")
         we = QWidgetAction(menu)
         we.setDefaultWidget(label)
@@ -117,12 +117,12 @@ class ComplexTemplateHandler(HtmlTemplateHandler):
     def prepareContextMenu(self, menu):
         HtmlTemplateHandler.prepareContextMenu(self, menu)
 
-        action = menu.addAction(QIcon(":/icons/images/collapse.png"), "Collapse %s" % self.varWrapper.getExp(), self.toggleCollapsed)
+        action = menu.addAction(QIcon(":/icons/images/collapse.png"), "Collapse %s" % self.varWrapper.exp, self.toggleCollapsed)
         action.setCheckable(True)
         action.setChecked(not self.varWrapper.isOpen)
 
         if self.varWrapper.isOpen:
-            action = menu.addAction(QIcon(":/icons/images/vertical.png"), "Vertical view for %s" % self.varWrapper.getExp(), self.toggleVertical)
+            action = menu.addAction(QIcon(":/icons/images/vertical.png"), "Vertical view for %s" % self.varWrapper.exp, self.toggleVertical)
             action.setCheckable(True)
             action.setChecked(self.vertical)
 
@@ -215,7 +215,7 @@ class ComplexDataGraphVW(DataGraphVW):
         DataGraphVW.__init__(self, variable, distributedObjects)
         self.isOpen = True
         self.vwFactory = vwFactory
-        self.children = None        # will be lazily evaluated once we need them
+        self.childrenWrapper = None        # will be lazily evaluated once we need them
         self.templateHandler = templateHandler
 
     def setOpen(self, open_):
@@ -225,10 +225,10 @@ class ComplexDataGraphVW(DataGraphVW):
     def getChildren(self):
         """ returns list of children as DataGraphVWs; creates the wrappers if they haven't yet been
         @return    list of datagraph.datagraphvw.DataGraphVW """
-        if not self.children:
-            self.children = []
-            for childVar in self.variable.getChildren():
+        if not self.childrenWrapper:
+            self.childrenWrapper = []
+            for childVar in self.childs:
                 wrapper = childVar.makeWrapper(self.vwFactory)
                 wrapper.setExistingView(self.getView(), self)
-                self.children.append(wrapper)
-        return self.children
+                self.childrenWrapper.append(wrapper)
+        return self.childrenWrapper

src/datagraph/htmlvariableview.py
@@ -61,7 +61,7 @@ class HtmlVariableView(QGraphicsWebView):
         self.dirty = True
 
         self.id = self.getUniqueId(self)
-        
+
         self.distributedObjects.signalProxy.variableUpdateCompleted.connect(self.render)
 
     def getIncomingPointers(self):
@@ -106,10 +106,10 @@ class HtmlVariableView(QGraphicsWebView):
         return self.source
 
     def openContextMenu(self, menu):
-        menu.addAction(QIcon(":/icons/images/minus.png"), 
-                "Remove %s" % self.varWrapper.getExp(), self.remove)
-        menu.addAction(QIcon(":/icons/images/save-html.png"), 
-                "Save HTML for %s" % self.varWrapper.getExp(), self.saveHtml)
+        menu.addAction(QIcon(":/icons/images/minus.png"),
+                "Remove %s" % self.varWrapper.exp, self.remove)
+        menu.addAction(QIcon(":/icons/images/save-html.png"),
+                "Save HTML for %s" % self.varWrapper.exp, self.saveHtml)
         menu.exec_(QCursor.pos())
 
     @QtCore.pyqtSlot()

src/datagraph/ptrvariableview.py
@@ -40,7 +40,7 @@ class PtrVariableTemplateHandler(HtmlTemplateHandler):
     def dereference(self):
         dgvw = self.varWrapper.dereference()
         if (dgvw != None):
-            logging.debug("dereferenced variable wrapper: new expression is %s", dgvw.getExp())
+            logging.debug("dereferenced variable wrapper: new expression is %s", dgvw.exp)
             self.distributedObjects.datagraphController.addVar(dgvw)
             self.distributedObjects.datagraphController.addPointer(self.varWrapper.getView(), dgvw.getView())
         else:
@@ -54,13 +54,13 @@ class PtrVariableTemplateHandler(HtmlTemplateHandler):
 
     def prepareContextMenu(self, menu):
         HtmlTemplateHandler.prepareContextMenu(self, menu)
-        menu.addAction("Dereference %s" % self.varWrapper.getExp(), self.dereference)
+        menu.addAction("Dereference %s" % self.varWrapper.exp, self.dereference)
         submenu = menu.addMenu("Show custom...")
 
         # we cannot construct the lineedit in our ctor since it will be automatically deleted once the menu is closed
         self.showCustomEdit = QLineEdit()
         self.showCustomEdit.returnPressed.connect(self.showCustom)
-        self.showCustomEdit.setText("*(%s)" % self.varWrapper.getExp())
+        self.showCustomEdit.setText("*(%s)" % self.varWrapper.exp)
         we = QWidgetAction(menu)
         we.setDefaultWidget(self.showCustomEdit)
         submenu.addAction(we)
@@ -84,8 +84,8 @@ class PtrDataGraphVW(DataGraphVW):
                     the Wrapper of the dereferenced Variable if the Variable can be dereferenced,
                     None otherwise
         """
-        dereferencedVar = self.variable.dereference()
-        if (dereferencedVar != None):
+        dereferencedVar = self._v.dereference()
+        if dereferencedVar is not None:
             return dereferencedVar.makeWrapper(self.factory)
         else:
             return None

src/datagraph/stdvariableview.py
@@ -37,7 +37,7 @@ class StdVariableTemplateHandler(HtmlTemplateHandler):
 
     def prepareContextMenu(self, menu):
         HtmlTemplateHandler.prepareContextMenu(self, menu)
-        filters.add_actions_for_all_filters(menu.addMenu(QIcon(":/icons/images/filter.png"), "Set Filter for %s..." % self.varWrapper.getExp()), self.varWrapper)
+        filters.add_actions_for_all_filters(menu.addMenu(QIcon(":/icons/images/filter.png"), "Set Filter for %s..." % self.varWrapper.exp), self.varWrapper)
 
 
 class StdDataGraphVW(DataGraphVW):

commit e5fdc27b8bf2214b4bddf66f617647416c90f02f

fix broken context menu and graphical view (bug introduced in commit 23303d2)

src/datagraph/arrayvariableview.py
@@ -112,7 +112,7 @@ class ArrayVariableTemplateHandler(ComplexTemplateHandler):
     def prepareContextMenu(self, menu):
         ComplexTemplateHandler.prepareContextMenu(self, menu)
 
-        graphicalViewPossible = all(isinstance(var, StdDataGraphVW) for var in self.varWrapper.children)
+        graphicalViewPossible = all(isinstance(var, StdDataGraphVW) for var in self.varWrapper.childrenWrapper)
 
         # we only allow the graphical view if all contained elements are standard variables; also,
         # do not show the menu if the variable view is collapsed
@@ -131,7 +131,7 @@ class ArrayVariableTemplateHandler(ComplexTemplateHandler):
 
     def plot(self, output):
         _importMatplotlib()        # only import matplotlib if we really need it
-        data = [float(var.getUnfilteredValue()) for var in self.varWrapper.children]
+        data = [float(var.unfilteredValue) for var in self.varWrapper.childrenWrapper]
 
         fig = plt.figure(figsize=(4, 3))
         ax = fig.add_subplot(111)