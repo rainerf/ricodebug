048449cfa37be3e27b05b2c6193b48891ebdd353
rainer <rainer@findenig.net>
2013-02-27 20:28:33 +0100

    allow multiple notifications

--- src/views/notificationframe.py
+++ src/views/notificationframe.py
@@ -1,5 +1,29 @@
+# ricodebug - A GDB frontend which focuses on visually supported
+# debugging using data structure graphs and SystemC features.
+#
+# Copyright (C) 2011  The ricodebug project team at the
+# Upper Austrian University Of Applied Sciences Hagenberg,
+# Department Embedded Systems Design
+#
+# This file is part of ricodebug.
+#
+# ricodebug is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# For further information see <http://syscdbg.hagenberg.servus.at/>.
+
 import logging
-from PyQt4.QtGui import QApplication, QStyle, QColor, QFrame
+from PyQt4.QtGui import QApplication, QStyle, QColor, QFrame, QVBoxLayout
 
 from views.ui_notificationframe import Ui_NotificationFrame
 
@@ -7,36 +31,32 @@ from views.ui_notificationframe import Ui_NotificationFrame
 class NotificationFrame(QFrame):
     INFO, WARNING, ERROR = range(3)
 
-    def __init__(self, parent):
+    def __init__(self, parent, message, severity):
         QFrame.__init__(self, parent)
         self.ui = Ui_NotificationFrame()
 
-        self._data = {
-            # self.POSITIVE: ("#6ebe8a", None),
-            self.INFO: ("#4398c8",
-                        self.palette().base().color().name(),
-                        QApplication.style().standardIcon(QStyle.SP_MessageBoxInformation).pixmap(36, 36)),
-            self.WARNING: ("#d0b05f",
-                           self.palette().text().color().name(),
-                           QApplication.style().standardIcon(QStyle.SP_MessageBoxWarning).pixmap(36, 36)),
-            self.ERROR: ("#cda8a8",
-                         self.palette().text().color().name(),
-                         QApplication.style().standardIcon(QStyle.SP_MessageBoxCritical).pixmap(36, 36))
-            }
+        if severity == self.INFO:
+            bgcolor = "#4398c8"
+            fgcolor = self.palette().base().color().name()
+            icon = QApplication.style().standardIcon(QStyle.SP_MessageBoxInformation).pixmap(36, 36)
+        elif severity == self.WARNING:
+            bgcolor = "#d0b05f"
+            fgcolor = self.palette().text().color().name()
+            icon = QApplication.style().standardIcon(QStyle.SP_MessageBoxWarning).pixmap(36, 36)
+        elif severity == self.ERROR:
+            bgcolor = "#cda8a8"
+            fgcolor = self.palette().text().color().name()
+            icon = QApplication.style().standardIcon(QStyle.SP_MessageBoxCritical).pixmap(36, 36)
 
         self.ui.setupUi(self)
-        self.hide()
+        self.ui.iconLabel.setPixmap(icon)
+        self.ui.messageLabel.setText(message)
+        self._setColor(bgcolor, fgcolor)
 
-    def setMessage(self, msg, severity):
-        self.ui.messageLabel.setText(msg)
-        self.setColor(self._data[severity][0], self._data[severity][1])
-        self.ui.iconLabel.setPixmap(self._data[severity][2])
-        self.show()
-
-    def setColor(self, bgcolor, fgcolor):
+    def _setColor(self, bgcolor, fgcolor):
         stylesheet = \
         """
-        #notificationFrame {
+        #NotificationFrame {
           border-radius: 5px;
           padding: 2px;
           background-color: QLinearGradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 %s, stop: 0.1 %s, stop: 1 %s);
@@ -56,14 +76,19 @@ class NotificationFrame(QFrame):
 
 
 class NotificationFrameHandler(logging.Handler):
-    def __init__(self, notificationFrame):
+    def __init__(self, notificationArea):
         logging.Handler.__init__(self)
-        self._notificationFrame = notificationFrame
+        self._notificationArea = notificationArea
+        self._notificationArea.setLayout(QVBoxLayout())
 
     def emit(self, record):
+        w = None
         if record.levelno >= logging.ERROR:
-            self._notificationFrame.setMessage(record.message, self._notificationFrame.ERROR)
+            w = NotificationFrame(self._notificationArea, record.message, NotificationFrame.ERROR)
         elif record.levelno >= logging.WARNING:
-            self._notificationFrame.setMessage(record.message, self._notificationFrame.WARNING)
+            w = NotificationFrame(self._notificationArea, record.message, NotificationFrame.WARNING)
         elif record.levelno >= logging.INFO:
-            self._notificationFrame.setMessage(record.message, self._notificationFrame.INFO)
+            w = NotificationFrame(self._notificationArea, record.message, NotificationFrame.INFO)
+
+        if w:
+            self._notificationArea.layout().addWidget(w)
